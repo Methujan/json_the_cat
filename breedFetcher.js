3805243e0e592da4a4d0202acafbdb1a1b89aefe
Methujan Jeyapalan <methujan_02@hotmail.com>
2021-06-03 23:02:50 +0000

    Refactored cat

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,21 @@
 const request = require('request');
 
-const fetchBreedDescription = function(breedName, callback){
+const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     if (error) {
       callback(`Failed: ${error}`, null);
     }
     
-    const data = JSON.parse(body);  
-    if (data[0]){
-      callback( null, data[0]['description']);
-      } 
+    const data = JSON.parse(body);
+    if (data[0]) {
+      callback(null, data[0]['description']);
+    }
     
-    if(!data[0]){
-        callback(`The requested breed: ${breedName} is not found`, null);
-      }
+    if (!data[0]) {
+      callback(`The requested breed: ${breedName} is not found`, null);
+    }
 
-  })
+  });
 };
 
 
--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const {fetchBreedDescription} = require('./breedFetcher');
 let breedName = process.argv[2];
 
 fetchBreedDescription(breedName, (error, desc) => {
-if (error) {
-  console.log(`Error fetch details: ${error}`);
+  if (error) {
+    console.log(`Error fetch details: ${error}`);
 
-} else {
-  console.log(desc)
+  } else {
+    console.log(desc);
 
-}
-})
+  }
+});
